1536bec207e85c5d88264f18a24ae70ec872b882
Leticia <Leticia@192.168.211.143>
2016-11-21 10:29:33 -0200

    Alteração de código X

---
+++ src/aula/Aula.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula;
+
+/**
+ *
+ * @author Leticia
+ */
+public class Aula {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean novaV = false;
+        int inteira = 2;
+        if (inteira < 1) {
+            novaV = true;
+        }
+        System.out.println("Variável: " + novaV);
+    }
+
+}
